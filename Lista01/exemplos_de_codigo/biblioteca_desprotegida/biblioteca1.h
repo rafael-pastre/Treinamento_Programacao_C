3375f7d1f19eca13ee44cbb65ca806cc0220bdd8
Rafael Pastre <44172578+rafael-pastre@users.noreply.github.com>
2020-04-01 18:51:53 -0300

    Add files via upload

---
+++ Lista01/exemplos_de_codigo/biblioteca_desprotegida/biblioteca1.h
@@ -0,0 +1,6 @@
+// Dependencias
+#include <stdio.h>
+#include <stdlib.h>
+
+// Cabecalhos das funcoes da biblioteca
+void funcao1();
---
+++ Lista01/exemplos_de_codigo/biblioteca_protegida/biblioteca1.h
@@ -0,0 +1,21 @@
+// Macro de protecao:
+#ifndef LIB_ZENITH_LIBNAME_H
+#define LIB_ZENITH_LIBNAME_H
+
+// Macro de compatiblidade para C++:
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Dependencias
+#include <stdio.h>
+#include <stdlib.h>
+
+// Cabecalhos das funcoes da biblioteca
+void funcao1();
+
+// Finalizacao dos macros
+#ifdef __cplusplus
+}
+#endif
+#endif//LIB_ZENITH_LIBNAME_H
